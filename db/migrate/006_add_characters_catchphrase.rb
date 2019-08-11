81616c5cd2fafa708a74cb5ae08e229e995f72bb
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-08-11 22:44:11 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_characters_catchphrase.rb
+++ db/migrate/006_add_characters_catchphrase.rb
@@ -1,9 +1,5 @@
-class CreateCharacters < ActiveRecord::Migration[5.1]
+class CreateCharactersCatchphrase < ActiveRecord::Migration[5.1]
   def change
-    create_table :characters do |t|
-      t.string :name
-      t.integer :actor_id
-      t.integer :show_id
-    end
+   add_column :ctachphrase
   end
 end
