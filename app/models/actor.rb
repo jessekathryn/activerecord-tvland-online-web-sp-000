ebbc27489e661faf8fb6b8510765f1c47bdcae4c
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-08-11 23:29:36 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,14 @@
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
+  
+  def full_name
+    self.first_name.last_name
+  end
+  
+  def shows_and_characters
+    self.shows.characters
+  end
 end
 
  
