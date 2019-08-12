ca68269771621483048301d370dfa3b8dd225385
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-08-12 01:03:51 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,7 +6,7 @@ class Character < ActiveRecord::Base
    "#{self.name} always says: #{self.catchphrase}"
  end 
  
- def build_show
+ def build_show(:name)
    show = Actor.find_or_create(:name)
    show.name
  end
