b6ffbaf49458035d72c56a432253a9f80d7fe766
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-08-12 00:17:55 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,11 +3,15 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
   
   def full_name
-     "#{self.first_name} #{self.last_name}"
-    end
+    "#{self.first_name} #{self.last_name}"
+  end
 
   def list_roles
-    "#{character.name} - #{show.name}"
+    peter = Actor.new(:first_name => "Peter", :last_name => 
+    "Drinklage") 
+    tyrion = Character.new(:name => "Tyrion Lannister")
+    tyrion.actor = peter 
+    thrones = Show.new(:name => "Game of Thrones")
   end
 end
 
